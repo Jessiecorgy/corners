073f52d17b6c2f4c546cb9ffc68be0ca800fff7c
Jessiecorgy <jessiecorgy@gmail.com>
2014-10-20 13:37:51 -0700

    initial

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+  $('#one').corner('rounded');
+  $('#two').corner('notch');
+  $('#three').corner('slide');
+
+});
+
